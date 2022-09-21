c45b96153e887139ff61a87c094312f63877637c
Kingsley Ohene <kingdela1@gmail.com>
2022-09-21 02:11:45 +0000

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-	print('{:c}'.format(i), end='')
+for n in range(97, 123):
+    if (n == 101 or n == 113):
+	continue
+    print("{}".format(chr(n)), end="")
