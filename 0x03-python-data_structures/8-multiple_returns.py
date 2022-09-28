42be4c5d40741a03eb733e7d1e42df1f3c5970eb
Kingsley Ohene <kingdela1@gmail.com>
2022-09-28 03:07:51 +0000

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	len_sentence = len(sentence)
+    len_sentence = len(sentence)
 
-	if (len_sentence == 0):
-		tuple = (len_sentence, None)
-	else:
-		tuple = (len_sentence, sentence[0])
+    if (len_sentence == 0):
+        tuple = (len_sentence, None)
+    else:
+        tuple = (len_sentence, sentence[0])
 
-	return (tuple)
+    return (tuple)
